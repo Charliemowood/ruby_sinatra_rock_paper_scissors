727dae5e7d8dfa7fa70e0b936bdada7632dcc516
Charlie Wood <charliemowood@gmail.com>
2017-05-01 22:40:44 +0100

    Add homepage with rules

--- controller.rb
+++ controller.rb
@@ -3,10 +3,14 @@ require('sinatra/contrib/all')
 require('pry-byebug')
 require_relative('./models/game')
 
-get '/win/:arg1/:arg2' do
+get '/play/:arg1/:arg2' do
   arg1 = params[:arg1]
   arg2 = params[:arg2]
   game = Game.new(arg1, arg2)
   @game = game.win()
   erb(:result)
+end
+
+get '/' do
+  erb(:home)
 end
