2e8c4d533d9bc7618e69dddeb67a8353de222930
Charlie Wood <charliemowood@gmail.com>
2017-05-01 21:44:41 +0100

    Improve win function

--- specs/game_spec.rb
+++ specs/game_spec.rb
@@ -6,10 +6,19 @@ class TestGame < MiniTest::Test
 
   def setup
     @game1 = Game.new('scissors', 'rock')
+    @game2 = Game.new('paper', 'rock')
   end
 
   def test_exists_class_rps
     refute_nil(@game1)
+  end
+
+  def test_scissor_rock
+    assert_equal('Rock wins!', @game1.win)
+  end
+
+  def test_paper_rock
+    assert_equal('Paper wins!', @game2.win)
   end
 
 end
