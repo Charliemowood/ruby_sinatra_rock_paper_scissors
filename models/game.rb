381567799f9a3bde55e170f404bf97b6055361db
Charlie Wood <charliemowood@gmail.com>
2017-05-01 23:08:37 +0100

    Add link to rules from result.erb

--- models/game.rb
+++ models/game.rb
@@ -10,6 +10,8 @@ class Game
        return "Paper wins!"
      elsif (@arg1 == 'paper' && @arg2 =='scissors') || (@arg1 == 'scissors' && @arg2 =='paper')
        return "Scissors wins!"
+     elsif (@arg1 == 'paper' && @arg2 =='paper') || (@arg1 == 'rock' && @arg2 =='rock') || (@arg1 == 'scissors' && @arg2 =='scissors')
+       return "That's a draw play again!"
      else
        return "Rock wins!"
      end
